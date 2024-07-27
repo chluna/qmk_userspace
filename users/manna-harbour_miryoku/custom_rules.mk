1f0941eca7e1f5cc6380a5c546d3687f5a36dfd0
Chester Luna <3890275+chluna@users.noreply.github.com>
2024-07-27 14:00:19 +0800

    Initial commit with miryoku

---
+++ users/manna-harbour_miryoku/custom_rules.mk
@@ -0,0 +1,31 @@
+# Copyright 2019 Manna Harbour
+# https://github.com/manna-harbour/miryoku
+
+# Custom shift keys
+# https://getreuer.info/posts/keyboards/custom-shift-keys/index.html
+SRC += features/custom_shift_keys.c
+
+# x-case
+# https://github.com/andrewjrae/kyria-keymap#x-case
+SRC += features/casemodes.c
+
+# Dalius-style one shot keys
+# https://blog.ffff.lt/posts/callum-layers/
+SRC += features/flow.c
+
+# Customize Miryoku
+MIRYOKU_ALPHAS = COLEMAKDH
+MIRYOKU_EXTRA = QWERTY
+MIRYOKU_TAP = QWERTY
+MIRYOKU_NAV = INVERTEDT
+MIRYOKU_CLIPBOARD = WIN
+
+# QMK features
+AUTO_SHIFT_ENABLE = no
+COMBO_ENABLE = yes
+KEY_OVERRIDE_ENABLE = no
+DYNAMIC_TAPPING_TERM_ENABLE = yes
+DYNAMIC_MACRO_ENABLE = yes
+OS_DETECTION_ENABLE = yes
+RGBLIGHT_SUPPORTED = no
+RGB_MATRIX_SUPPORTED = no
